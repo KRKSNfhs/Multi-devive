c6bc61f54ac5d94bec48a0886609643f7066c758
KRKSNfhs <matesanelnuean@gmail.com>
2025-05-16 10:23:05 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -11,7 +11,7 @@ cmd({
 async(robin, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
     await robin.sendPresenceUpdate('recording', from);
-    await robin.sendMessage(from, { audio: { url: "https://github.com/pradipkavi/KAVI-MD-V1/raw/refs/heads/main/My/ASLAVA%20FUNK!%20(1).mp3" }, mimetype: 'audio/mpeg', ptt: true }, { quoted: mek });
+    await robin.sendMessage(from, { audio: { url: "https://files.catbox.moe/3g44u0.mpeg" }, mimetype: 'audio/mpeg', ptt: true }, { quoted: mek });
 return await robin.sendMessage(from,{image: {url: config.ALIVE_IMG},caption: config.ALIVE_MSG},{quoted: mek})
     
 }catch(e){
